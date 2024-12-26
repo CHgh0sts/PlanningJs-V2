9df849ebadee6330796c189d089fa49fc0bb78ca
CHgh0sts <chghosts.dev@gmail.com>
2024-12-26 23:29:17 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,14 @@
 import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  })({
-    reactStrictMode: true,
-    experimental: {
-      appDir: false,
-    },
-  });
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-export default nextConfig;
+export default {
+  ...nextConfig,
+  reactStrictMode: true,
+  experimental: {}
+};
